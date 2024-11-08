a6732d6d808ed358e730135691857f82a07cb983
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-08 14:08:15 +0100

    select-dom

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// DOM Elements
+const postItContainerEl = document.querySelector("#notesContainer");
+const btnCloseLayoverEl = document.querySelector("#Closebtn");
+const layoverImage = document.querySelector("#Imageover");
