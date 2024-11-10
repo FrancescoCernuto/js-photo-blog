b9e5546d62e847b3a8f1d6f85b7cfd514ebcdde1
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-10 16:53:04 +0100

    add-await fetch

--- js/main.js
+++ js/main.js
@@ -6,5 +6,7 @@ const layoverEl = document.querySelector("#layover");
 
 let imageData = [];
 
-
+const loadImages = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/photos?_limit=6");
 
