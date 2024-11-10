65c42ab1b536fb4fcc4d361023cfd279c029fc1e
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-10 16:54:38 +0100

    finish await

--- js/main.js
+++ js/main.js
@@ -10,3 +10,5 @@ const loadImages = async () => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/photos?_limit=6");
 
+        imageData = await response.json();
+    } 
