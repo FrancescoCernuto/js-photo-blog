76658e1d63861cf89efd8f310f134bc19d04b5bd
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-14 11:58:16 +0100

    add-generetor-cards

--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ const overlayEl = document.getElementById("overlay");
 const overlayBtn = document.getElementById("overlay-btn");
 const overlayImg = document.getElementById("overlay-img");
 
+const cardToGen = 6; 
