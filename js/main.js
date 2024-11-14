39163facbf5b99dab24de18b2bf882d981a31dfe
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-14 12:33:59 +0100

    add-funct overlayHandler & use foreach

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ const overlayBtn = document.getElementById("overlay-btn");
 const overlayImg = document.getElementById("overlay-img");
 
 const cardToGen = 6; 
+
+const overlayHandler = () => {
+    const allCards = document.querySelectorAll(".card");
+    allCards.forEach((singleCard) => {
+        const imagesEl = singleCard.children[1].firstElementChild;
+
+        
